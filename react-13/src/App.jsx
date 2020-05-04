db59a6511618a9d4b2bc6f70bc6a6db4318bc4a5
Arilson Souza <arilsons61@gmail.com>
2020-05-04 18:49:34 -0300

    Refactor Topbar component

--- react-13/src/App.jsx
+++ react-13/src/App.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 
-import { ReactComponent as LogoSvg } from "./assets/img/logo.svg";
-
 import './App.scss';
+import Topbar from './components/Topbar';
 
 class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <header className="topbar">
-          <div className="container">
-            <a href="/" className="topbar__logo">
-              <LogoSvg alt="Logo Instagram" />
-            </a>
-          </div>
-        </header>
+        <Topbar/>
 
         <div className="container">
           <section className="filters">
