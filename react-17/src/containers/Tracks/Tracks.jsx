a29dab278f17756fdd07ebddefa9d06515ab2632
Arilson Souza <arilsons61@gmail.com>
2020-06-02 19:05:08 -0300

    Added data tests ids

--- react-17/src/containers/Tracks/Tracks.jsx
+++ react-17/src/containers/Tracks/Tracks.jsx
@@ -2,7 +2,7 @@ import React  from 'react';
 
 import './Tracks.scss';
 
-const Tracks = ({ categoryName, data, isLoading, path }) => (<div className="tracks" data-testid="tracks" />);
+const Tracks = ({ categoryName, data, isLoading, path }) => (<div className="tracks" data-testid="tracks" data-testid="tracks"/>);
 
 export default Tracks;
 
