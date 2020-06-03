c38c3e768eb5e420a7f8a099082bc68c48f55337
Arilson Souza <arilsons61@gmail.com>
2020-06-03 20:19:51 -0300

    Setup WelcomeBox and RouteHeader components

--- react-17/src/components/WelcomeBox/WelcomeBox.jsx
+++ react-17/src/components/WelcomeBox/WelcomeBox.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './WelcomeBox.scss';
 
 const WelcomeBox = ({ name }) => (
-  <div className="welcome-box" data-testid="welcome-box"/>
+  <div className="welcome-box" data-testid="welcome-box">
+    <div className="container">
+      <span>Bem vindo <strong>{name}</strong></span>
+    </div>
+  </div>
 );
 
 export default WelcomeBox;
